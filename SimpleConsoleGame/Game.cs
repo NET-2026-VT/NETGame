23d57889a20f20cb56861e213cd088d169fcb9f4
Jonathan Krall <jonathan.krall@lexicon.se>
2026-05-27 13:28:30 +0200

    Added Player

--- SimpleConsoleGame/Game.cs
+++ SimpleConsoleGame/Game.cs
@@ -1,16 +1,41 @@
 ﻿internal class Game
 {
+    private Map _map = null!;
+    private Player _player = null!;
     public Game()
     {
     }
 
     internal void Run()
     {
-        Init(); 
+        Init();
+        Play();
+    }
+
+    private void Play()
+    {
+        bool gameInProgress = true; 
+        do
+        {
+            //DrawMap
+
+            //GetCommand
+
+            //Act
+
+            //DrawMap
+
+            //EnemyAction
+
+            //DrawMap
+
+
+        } while (gameInProgress);
     }
 
     private void Init()
     {
-        var map = new Map(height:10, width: 10); 
+        _map = new Map(height:10, width: 10);
+        _player = new Player(); 
     }
 }
