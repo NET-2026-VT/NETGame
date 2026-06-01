870dd89aa00206433e55e2abe52c835f19fdb2e0
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-01 13:56:07 +0200

    Start on LimitedList

---
+++ SimpleConsoleGame.LimitedList/LimitedList.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleConsoleGame.LimitedList;
+
+public class LimitedList<T> 
+{
+    private List<T> _list;
+    private int _capacity;
+
+    public int Count => _list.Count;
+    public bool IsFull => _capacity <= Count;
+
+    public LimitedList(int capacity)
+    {
+        _capacity = Math.Max(capacity, 2);
+        _list = new List<T>(_capacity);
+    }
+
+    public bool Add(T item)
+    {
+        if(IsFull) return false;
+        _list.Add(item); return true;
+    }
+}
--- SimpleConsoleGame/Program.cs
+++ SimpleConsoleGame/Program.cs
@@ -1,5 +1,11 @@
-﻿var li = new List<int>();
+﻿using SimpleConsoleGame.LimitedList;
+
+var li = new List<int>();
 var creatureList = new List<Creature>();
+
+var ll = new LimitedList<int>(2);
+var ll2 = new LimitedList<Game>(4);
+
 
 var game = new Game();
 
