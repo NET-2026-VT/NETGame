2d5ecc1c6b1719b93981c01b8543d372ac90dc55
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-03 12:08:56 +0200

    Remove old demos from Program

--- SimpleConsoleGame/Program.cs
+++ SimpleConsoleGame/Program.cs
@@ -3,47 +3,6 @@ using SimpleConsoleGame.Extensions;
 using SimpleConsoleGame.GameWorld;
 using SimpleConsoleGame.LimitedList;
 
-//var li = new List<int>();
-
-//foreach (var item in li)
-//{
-//    Console.WriteLine(item);
-//}
-
-//var name = "Kalle";
-
-//foreach (var item in name)
-//{
-//    Console.WriteLine(item);
-//}
-
-//var creatureList = new List<Creature>();
-//var playerarray = new Player[5];
-
-//foreach (var item in playerarray)
-//{
-//    Console.WriteLine(item);
-//}
-
-//var res = creatureList.CreatureAt(new Cell(new Position()));
-
-var llllllll = new LimitedList<int>(2);
-
-var count = llllllll.Count;
-var count2 = llllllll.Count();
-
-
-
-foreach (var item in llllllll)
-{
-    Console.WriteLine(item);
-}
-
-
-
-//var ll2 = new LimitedList<Game>(4);
-
-
 var game = new Game();
 
 game.Run(); 
