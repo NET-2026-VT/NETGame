5046b6028eebd1a81e9e442b6ddd4976ab45281e
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-04 09:47:34 +0200

    Set  MaxHealth and Damage on Player

--- SimpleConsoleGame/Characters/Player.cs
+++ SimpleConsoleGame/Characters/Player.cs
@@ -9,10 +9,11 @@ namespace SimpleConsoleGame
     internal class Player : Creature
     {
         public LimitedList<Item> BackPack { get; }
-        public Player(Cell cell) : base(cell, "P ")
+        public Player(Cell cell) : base(cell, "P ", maxHealth: 100)
         {
             Color = ConsoleColor.White;
             BackPack = new LimitedList<Item>(3);
+            Damage = 100;
         }
     }
 }
