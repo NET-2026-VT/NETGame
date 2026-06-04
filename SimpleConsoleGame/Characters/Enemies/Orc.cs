9ed79186ddeb58e995eb411a7787e9f1075158a4
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-04 09:21:02 +0200

    Add some enemies

---
+++ SimpleConsoleGame/Characters/Enemies/Orc.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleConsoleGame.Characters.Enemies;
+
+internal class Orc : Creature
+{
+    public Orc(Cell cell) : base(cell, "O "){}
+}
