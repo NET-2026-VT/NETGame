7c3cb1f8c1a08df7910eafcc03b118c334e1119e
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-04 11:20:29 +0200

    Add IEnumerable ForEach

---
+++ SimpleConsoleGame/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleConsoleGame.Extensions;
+
+internal static class IEnumerableExtensions
+{
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source)
+        {
+            action?.Invoke(item);
+        }
+    }
+}
