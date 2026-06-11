4bf1a0a9ed03a8086f20c25fc2f70f1d2be06ed1
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-11 12:08:56 +0200

    Map test with wrapper class

--- SimpleConsoleGame.Tests/MapTests.cs
+++ SimpleConsoleGame.Tests/MapTests.cs
@@ -1,10 +1,27 @@
-﻿namespace SimpleConsoleGame.Tests;
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using SimpleConsoleGame.Extensions;
+
+namespace SimpleConsoleGame.Tests;
 
 public class MapTests
 {
     [Fact]
-    public void Test1()
+    public void Constructor_SetCorrectWidth_WithIConfig()
     {
+        //Arrange
+        const int expectedWidth = 10;
 
+        var iconfigMock = new Mock<IConfiguration>();
+        var getMapSizeForMock = new Mock<IGetMapSize>();
+
+        getMapSizeForMock.Setup(x => x.GetMapSizeFor(iconfigMock.Object, It.IsAny<string>())).Returns(expectedWidth);
+        GetMapSizeForWrapper.Implementation = getMapSizeForMock.Object;
+
+        //Act
+        var map = new Map(iconfigMock.Object);
+
+        //Assert
+        Assert.Equal(expectedWidth, map.Width);
     }
 }
--- SimpleConsoleGame/Extensions/ConfigurationExtensions.cs
+++ SimpleConsoleGame/Extensions/ConfigurationExtensions.cs
@@ -5,11 +5,38 @@ using System.Text;
 
 namespace SimpleConsoleGame.Extensions;
 
-internal static class ConfigurationExtensions
+//internal static class ConfigurationExtensions
+//{
+//    public static int GetMapSizeFor(this IConfiguration config, string key)
+//    {
+//        var section = config.GetSection("game:mapsettings");
+//        return int.TryParse(section[key], out int result) ? result : throw new ArgumentException();
+//    }
+//}
+
+public static class GetMapSizeForWrapper
 {
+    public static IGetMapSize Implementation { private get; set; } = new GetMapSize();
     public static int GetMapSizeFor(this IConfiguration config, string key)
+    {
+        return Implementation.GetMapSizeFor(config, key);
+    }
+
+}
+
+public interface IGetMapSize
+{
+    int GetMapSizeFor(IConfiguration config, string key);
+}
+
+public class GetMapSize : IGetMapSize
+{
+    public int GetMapSizeFor(IConfiguration config, string key)
     {
         var section = config.GetSection("game:mapsettings");
         return int.TryParse(section[key], out int result) ? result : throw new ArgumentException();
     }
 }
+
+
+
