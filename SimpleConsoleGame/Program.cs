b1601456e6c7f4aac46c266c5d8ae3258b14394f
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-11 10:43:44 +0200

    Start on setup Benchmark

--- SimpleConsoleGame/Program.cs
+++ SimpleConsoleGame/Program.cs
@@ -1,39 +1,14 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using BenchmarkDotNet.Running;
 using SimpleConsoleGame;
-using SimpleConsoleGame.Extensions;
-using SimpleConsoleGame.GameWorld;
-using SimpleConsoleGame.LimitedList;
+using SimpleConsoleGame.BenchMarks;
 
 
-//IConfiguration config = new ConfigurationBuilder()
-//                                .SetBasePath(Environment.CurrentDirectory)
-//                                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-//                                .Build();
+#if BENCHMARK
+BenchmarkRunner.Run<InventoryBenchMarks>();
+#else
 
-var host = Host.CreateDefaultBuilder(args)
-               .ConfigureServices(services =>
-               {
-                   services.AddSingleton<IConsoleUI, ConsoleUI>();
-                   services.AddSingleton<IMap, Map>();
-                  // services.AddSingleton<IConfiguration>(config);
-                   services.AddSingleton<Game>();
-                   services.AddSingleton<ILimitedList<string>>(new MessageLog<string>(6));
-                   services.AddSingleton<ILimitedList<Item>>(new LimitedList<Item>(3));
-               })
-               .UseConsoleLifetime()
-               .Build();
-
-host.Services.GetRequiredService<Game>().Run();
-
-
-//var x = config.GetMapSizeFor("x");
-//var y = config.GetMapSizeFor("y");
-//var map = new Map(height: y , width: x);
-//var ui = new ConsoleUI(map);
-//var game = new Game(new ConsoleUI(new Map(new )), map);
-
-//game.Run();
+new SetUp().SetUpGame();
 
 Console.WriteLine("Game Over");
+
+#endif
---
+++ SimpleConsoleGame/SetUp.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleConsoleGame.GameWorld;
+using SimpleConsoleGame.LimitedList;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleConsoleGame;
+
+internal class SetUp
+{
+    public void SetUpGame()
+    {
+        var host = Host.CreateDefaultBuilder()
+               .ConfigureServices(services =>
+               {
+                   services.AddSingleton<IConsoleUI, ConsoleUI>();
+                   services.AddSingleton<IMap, Map>();
+                   // services.AddSingleton<IConfiguration>(config);
+                   services.AddSingleton<Game>();
+                   services.AddSingleton<ILimitedList<string>>(new MessageLog<string>(6));
+                   services.AddSingleton<ILimitedList<Item>>(new LimitedList<Item>(3));
+               })
+               .UseConsoleLifetime()
+               .Build();
+
+        host.Services.GetRequiredService<Game>().Run();
+    }
+}
