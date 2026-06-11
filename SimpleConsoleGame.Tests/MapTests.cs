355cd50f93893d27dffa13efd631e75ab6be3584
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-11 13:27:03 +0200

    Func

--- SimpleConsoleGame.Tests/MapTests.cs
+++ SimpleConsoleGame.Tests/MapTests.cs
@@ -6,17 +6,33 @@ namespace SimpleConsoleGame.Tests;
 
 public class MapTests
 {
+    //[Fact]
+    //public void Constructor_SetCorrectWidth_WithIConfig()
+    //{
+    //    //Arrange
+    //    const int expectedWidth = 10;
+
+    //    var iconfigMock = new Mock<IConfiguration>();
+    //    var getMapSizeForMock = new Mock<IGetMapSize>();
+
+    //    getMapSizeForMock.Setup(x => x.GetMapSizeFor(iconfigMock.Object, It.IsAny<string>())).Returns(expectedWidth);
+    //    GetMapSizeForWrapper.Implementation = getMapSizeForMock.Object;
+
+    //    //Act
+    //    var map = new Map(iconfigMock.Object);
+
+    //    //Assert
+    //    Assert.Equal(expectedWidth, map.Width);
+    //}
+
     [Fact]
-    public void Constructor_SetCorrectWidth_WithIConfig()
+    public void Constructor_SetCorrectWidth_WithFunc()
     {
         //Arrange
         const int expectedWidth = 10;
 
         var iconfigMock = new Mock<IConfiguration>();
-        var getMapSizeForMock = new Mock<IGetMapSize>();
-
-        getMapSizeForMock.Setup(x => x.GetMapSizeFor(iconfigMock.Object, It.IsAny<string>())).Returns(expectedWidth);
-        GetMapSizeForWrapper.Implementation = getMapSizeForMock.Object;
+        ConfigurationExtensions2.Implementation = (config, key) => expectedWidth;        
 
         //Act
         var map = new Map(iconfigMock.Object);
--- SimpleConsoleGame/Extensions/ConfigurationExtensions.cs
+++ SimpleConsoleGame/Extensions/ConfigurationExtensions.cs
@@ -14,29 +14,42 @@ namespace SimpleConsoleGame.Extensions;
 //    }
 //}
 
-public static class GetMapSizeForWrapper
+internal static class ConfigurationExtensions2
 {
-    public static IGetMapSize Implementation { private get; set; } = new GetMapSize();
-    public static int GetMapSizeFor(this IConfiguration config, string key)
-    {
-        return Implementation.GetMapSizeFor(config, key);
-    }
-
-}
-
-public interface IGetMapSize
-{
-    int GetMapSizeFor(IConfiguration config, string key);
-}
-
-public class GetMapSize : IGetMapSize
-{
-    public int GetMapSizeFor(IConfiguration config, string key)
+    public static Func<IConfiguration, string, int> Implementation { private get; set; } = (config, key) =>
     {
         var section = config.GetSection("game:mapsettings");
         return int.TryParse(section[key], out int result) ? result : throw new ArgumentException();
+    };
+    public static int GetMapSizeFor(this IConfiguration config, string key)
+    {
+        return Implementation(config, key);
     }
 }
+
+//public static class GetMapSizeForWrapper
+//{
+//    public static IGetMapSize Implementation { private get; set; } = new GetMapSize();
+//    public static int GetMapSizeFor(this IConfiguration config, string key)
+//    {
+//        return Implementation.GetMapSizeFor(config, key);
+//    }
+
+//}
+
+//public interface IGetMapSize
+//{
+//    int GetMapSizeFor(IConfiguration config, string key);
+//}
+
+//public class GetMapSize : IGetMapSize
+//{
+//    public int GetMapSizeFor(IConfiguration config, string key)
+//    {
+//        var section = config.GetSection("game:mapsettings");
+//        return int.TryParse(section[key], out int result) ? result : throw new ArgumentException();
+//    }
+//}
 
 
 
